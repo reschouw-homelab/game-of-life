11d6187713079edb4a26327c6a8e57fca4b8f2f6
Ryan Schouweiler <reschouw@gmail.com>
2022-04-29 19:40:53 -0700

    Messing with board naming

--- test_board.py
+++ test_board.py
@@ -1,27 +1,27 @@
-import board
+import board as _board
 import pytest
 
 def test_create_board():
-    board = board.board()
+    board = _board.board()
     assert(board.is_empty)
 
 def test_get_board():
-    board = board.board()
+    board = _board.board()
     empty_state = [[False for x in range(10)] for y in range(10)]
     assert(board.get() == empty_state)
 
 def test_print_board():
-    board = board.board(2,2)
+    board = _board.board(2,2)
     board.print()
 
 def test_is_empty():
-    board = board.board()
+    board = _board.board()
     assert(board.is_empty())
     board.set_cell(1,1, True)
     assert(not board.is_empty())
 
 def test_cell():
-    board = board.board()
+    board = _board.board()
     board.set_cell(0,0,True)
     assert(board.get_cell(0,0) == True)
     assert(board.get_cell(1,0) == False)
