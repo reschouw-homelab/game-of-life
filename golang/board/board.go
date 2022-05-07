25fd4d3213855ec1f628929d3c8c1c67563143f7
Ryan Schouweiler <reschouw@gmail.com>
2022-05-07 08:40:22 -0700

    getting and setting cells now working

--- golang/board/board.go
+++ golang/board/board.go
@@ -11,5 +11,21 @@ func CreateBoard(width int, height int) Board {
 }
 
 func IsEmpty(board Board) bool {
+	for x := 0; x < len(board); x++ {
+		for y := 0; y < len(board[x]); y++ {
+			if board[x][y] {
+				return false
+			}
+		}
+	}
 	return true
+}
+
+func SetCell(board Board, x int, y int, state bool) Board {
+	board[x][y] = state
+	return board
+}
+
+func GetCell(board Board, x int, y int) bool {
+	return board[x][y]
 }
--- golang/board/board_test.go
+++ golang/board/board_test.go
@@ -1,7 +1,6 @@
 package board
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,7 +8,17 @@ import (
 
 func TestEmpty(t *testing.T) {
 	board := CreateBoard(10, 10)
-	fmt.Println(board)
-
 	assert.Equal(t, IsEmpty(board), true, "Expecting board to be empty by default")
+	board = SetCell(board, 5, 5, true)
+	assert.Equal(t, IsEmpty(board), false, "Expecting board to not be empty")
+}
+
+func TestGetSet(t *testing.T) {
+	board := CreateBoard(10, 10)
+	board = SetCell(board, 5, 5, true)
+	board = SetCell(board, 0, 0, true)
+	board = SetCell(board, 9, 9, true)
+	assert.Equal(t, GetCell(board, 3, 3), false, "Expecting 3,3 to be empty")
+	assert.Equal(t, GetCell(board, 0, 0), true, "Expecting 0,0 to be alive")
+	assert.Equal(t, GetCell(board, 9, 9), true, "Expecting 9,9 to be alive")
 }
